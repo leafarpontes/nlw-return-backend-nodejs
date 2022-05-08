28b44c497f85fb584650716070d5297962960d41
leafarpontes <rafapontes2008@hotmail.com>
2022-05-08 18:39:06 -0300

    Adicionado envio de e-mail com Mailtrap

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "4aad45e55a1515",
+    pass: "b9967fa50c4426"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -15,6 +25,18 @@ app.post('/feedbacks', async (req, res) => {
       comment: comment,
       screenshot: screenshot
     }
+  });
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Seu Nome <seuemail@mail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
   })
 
   return res.status(201).json({data: feedback});
